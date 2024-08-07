97071c4c4322a65ced6c4f6969a3d36fe3c1a3e9
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-08-07 15:46:19 +0200

    Variable correction & License Addition

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 
 # Spec.0 License 2024 Andres Rulsan Abadias Otal
 
-# Otorgar permisos al software
-
-
-
 # Rutas del software
 # Ruta del directorio donde se encuentra el script de instalación
 install_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -15,8 +11,19 @@ service_location="/etc/systemd/system"
 service_name="ssp.service"
 # Recarga, Habilitacion y Estado del demonio
 reload_damon="sudo systemctl daemon-reload"
+unmask_daemon="sudo systemctl unmask ssp.service"
 enable_daemon="sudo systemctl enable ssp.service"
 status_daemon="sudo systemctl status ssp.service"
+
+# Limpiar consola
+clear
+
+# Mostrar licencia
+echo "By using the software, you agree to abide by the terms of the Spec.0 License."
+read -p "Do you want to read the full license? [y/n]: " license_bypass
+if [[ $license_bypass == "y" ]]; then
+    sudo less "$install_dir/LICENSE.md"
+fi
 
 # Montar, instalar y documentar
 if [[ -f "$install_dir/ssp_/systemd_file/$service_name" ]]; then
@@ -24,6 +31,7 @@ if [[ -f "$install_dir/ssp_/systemd_file/$service_name" ]]; then
     if [[ -f $service_location/$service_name ]]; then
         # Habilitar servicio
         $reload_damon
+        $unmask_daemon
         $enable_daemon
         read -p "Do you want to see ssp status [y/n]: " ssp_satus
         if [[ $ssp_satus == "y" ]]; then
