8a485b0debbfc69a1ad465bd256f13460769f327
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-08-29 12:46:23 +0200

    ssp  value

--- ssp_/ssp.sh
+++ ssp_/ssp.sh
@@ -8,13 +8,13 @@ ALLOWED_SERVICES="/etc/ssp/permitted_services.txt"
 # ssp -d -> Desinstalar servicio y relacionados
 # ssp -s -> Mostrar servicios permitidos (con less)
 
-if [[ $2 == "-a" ]]; then
+if [[ $1 == "-a" ]]; then
     sudo echo $2 >> "$ALLOWED_SERVICES"
     echo "Servicio $2 añadido a la lista permitida"
-elif [[ $2 == "-r" ]]; then
+elif [[ $1 == "-r" ]]; then
     sudo sed -i "/$2/d" "$ALLOWED_SERVICES"
     echo "Servicio $2 eliminado de la lista permitida"
-elif [[ $2 == "-d" ]]; then
+elif [[ $1 == "-d" ]]; then
     echo "Desinstalando programa..."
 else
      echo "Uso: $0 { -a (Add service to list) | -r (Remove service from list) | -d (Uninstall software) } servicio"
