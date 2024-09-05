e593681c06ffb72a1fdccea7d3aa6455d41ea6b3
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-09-05 10:11:08 +0200

    Installation Variant 2

--- install2.sh
+++ install2.sh
@@ -11,3 +11,17 @@ install_dir_sbin="/usr/local/sbin"
 
 # Instalacion de fichero ejecutable
 
+# Ruta /user/local/sbin/
+
+# Clonacion de fichero ssp.sh
+cp "$install_dir/ssp_/ssp.sh" "$install_dir_bin/ssp"
+
+# Mensajes de depuracion
+echo "$install_dir/ssp_/ssp.sh"
+echo "$install_dir_bin/ssp"
+
+# Creacion de /usr/local/sbin/ssp/ como directorio
+sudo mkdir "$install_dir_bin/ssp"
+echo "$install_dir_bin/ssp"
+
+ls -l "/usr/local/sbin/"
