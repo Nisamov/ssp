303ba9ee82538a5ee096fae10b27b505b8c07bda
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-09-07 04:10:06 +0200

    Installation Variant Test

--- install2.sh
+++ install2.sh
@@ -7,23 +7,8 @@ install_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 service_location="/usr/lib/systemd/system/"
 service_name="ssp.service"
 allowed_services="/etc/ssp/permitted_services.txt"
-install_dir_sbin="/usr/local/sbin"
 
 # Instalacion de fichero ejecutable
 
-# Ruta /user/local/sbin/
-
-# Creacion de /usr/local/sbin/ssp/ como directorio
-sudo mkdir "$install_dir_bin/ssp"
-echo "$install_dir_bin/ssp"
-
-if [[ -d "$install_dir_bin/ssp" ]]; then
-    echo "ok Dir"
-fi
-
-# Clonacion de fichero ssp.sh
-cp "$install_dir/ssp_/ssp.sh" "$install_dir_bin/ssp"
-
-if [[ -f "$install_dir_bin/ssp" ]]; then
-    echo "ok File"
-fi
+sudo mkdir "/usr/local/sbin/ssp_"
+sudo mv "$install_dir/ssp_/ssp.sh" "/usr/local/sbin/ssp"
