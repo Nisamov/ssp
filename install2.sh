483550723a71433f72a2c0d6430e0bc122408612
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-09-07 04:30:08 +0200

    Installation Variant Test 3

--- install2.sh
+++ install2.sh
@@ -7,23 +7,44 @@ install_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 service_location="/usr/lib/systemd/system/"
 service_name="ssp.service"
 allowed_services="/etc/ssp/permitted_services.txt"
+# Gestion del servicio
+reload_daemon="sudo systemctl daemon-reload"
+unmask_daemon="sudo systemctl unmask ssp.service"
+enable_daemon="sudo systemctl enable ssp.service"
+status_daemon="sudo systemctl status ssp.service"
+# Definimos el tamaño total de la barra de progreso
+TOTAL=50
+# Inicializamos la variable progreso
+progreso=0
+# Función para mostrar la barra de progreso
+mostrar_barra_progreso() {
+    # Calcula el número de almohadillas y guiones que se deben mostrar
+    completado=$((progreso * TOTAL / 100))
+    faltante=$((TOTAL - completado))
 
+    # Construye la barra de progreso con almohadillas (#) y guiones (-)
+    barra=$(printf "%0.s#" $(seq 1 $completado))
+    barra+=$(printf "%0.s-" $(seq 1 $faltante))
 
-function progreso () #crear una funcion (progreso)
-{
-typeset -i i=0
-while [ $i -le "20" ] #creamos el ciclo
-do
-echo -e "\033[44m\033[30m \033[0m \\c" #coloca un espacio en color azul que indique la barra de progreso
-sleep 1 #damos un intervalo de 1 seg
-i=i+1
-done
+    # Muestra la barra de progreso con el porcentaje completado
+    printf "\r[%s] %d%%" "$barra" "$progreso"
 }
-#####Main#####
-progreso # llamamos la funcion dentro del shell
+# Función para aumentar el progreso
+incrementar_progreso() {
+    paso=$1
+    progreso=$((progreso + paso))
+    if [ "$progreso" -gt 100 ]; then
+        progreso=100
+    fi
+    mostrar_barra_progreso
+}
 
+# Limpiar consola
+clear
 
-
+# Simulación de tareas en el script
+echo "Installing dependences..."
+sleep 1  # Simulación del tiempo de ejecución
 
 # Creacion de directorios del servicio
 #   Directorio de subprogramas
@@ -36,3 +57,24 @@ sudo mv "$install_dir/ssp_/ssp.sh" "/usr/local/sbin/ssp"
 
 # Otorgar permisos al script
 sudo chmod 777 "/usr/local/sbin/ssp"
+
+# Instalacion de licencia
+sudo mv "$install_dir/LICENSE.md" "/usr/local/sbin/ssp_/LICENSE.md"
+
+# Incrementa el progreso en 10%
+incrementar_progreso 10
+sleep 1  # Simulación del tiempo de ejecución
+
+# Creacion de ruta para scripts python
+sudo mkdir "/usr/local/sbin/ssp_/py_service"
+
+# Clonacion servicio y otorgacion de servicios
+sudo cp "$install_dir/ssp_/python_service/ssp.service.py" "/usr/local/sbin/ssp_/py_service/ssp.service.py"
+sudo chmod +x "/usr/local/sbin/ssp_/py_service/ssp.service.py"
+
+# Incrementa el progreso en 10%
+incrementar_progreso 10
+sleep 1  # Simulación del tiempo de ejecución
+
+# Llamar al generador de servicio
+sudo bash "$install_dir/ssp_/bash_file/systemd_contruct.sh"
