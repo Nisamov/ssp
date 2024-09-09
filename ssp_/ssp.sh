1f31a491ded781520390a444deea1d33141585df
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-09-09 02:43:34 +0200

    ssp.sh fixed

--- ssp_/ssp.sh
+++ ssp_/ssp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ALLOWED_SERVICES="/etc/ssp/permitted_services.txt"
+allowed_services="/etc/ssp/allowed_services.txt"
 
 # Ejemplo de ejcucion:
 # ssp -a -> Añadir servicio
@@ -8,20 +8,22 @@ ALLOWED_SERVICES="/etc/ssp/permitted_services.txt"
 # ssp -d -> Desinstalar servicio y relacionados
 # ssp -s -> Mostrar servicios permitidos (con less)
 
-if [[ $1 == "-a" ]]; then
+if [[ $1 == "-a" || $1 == "--add" ]]; then
     # Confirmacion
     read -p "Are you sure you want to add '$2' to whitelist? [y/n]: " newserviceaccept
     if [[ $newserviceaccept == "y" ]]; then
-        sudo echo $2 >> "$ALLOWED_SERVICES"
+        sudo echo $2 >> "$allowed_services"
         echo "'$2' service added to whitelist."
     else
         echo "Action cancelled."
     fi
-elif [[ $1 == "-r" ]]; then
-    sudo sed -i "/$2/d" "$ALLOWED_SERVICES"
+elif [[ $1 == "-r" || $1 == "--remove" ]]; then
+    sudo sed -i "/$2/d" "$allowed_services"
     echo "Servicio $2 eliminado de la lista permitida"
-elif [[ $1 == "-d" ]]; then
+elif [[ $1 == "-u" || $1 == "--uninstall" ]]; then
     echo "Desinstalando programa..."
+elif [[ $1 == "-h" || $1 == "--help" ]]; then
+    echo "Use: $0 { -a / --add (Add service to list) | -r / --remove (Remove service from list) | -u / --uninstall (Uninstall software) | -h / --help (show this help)}."
 else
-     echo "Uso: $0 { -a (Add service to list) | -r (Remove service from list) | -d (Uninstall software) } servicio"
+    echo "Use: $0 { -a / --add (Add service to list) | -r / --remove (Remove service from list) | -u / --uninstall (Uninstall software) | -h / --help (show this help)}."
 fi
