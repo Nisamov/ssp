6bb58caffcd40eaa68051987612620cb366ad3cf
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-09-09 03:01:12 +0200

    Fix ssp_uninstall

--- ssp_/ssp_uninstall.sh
+++ ssp_/ssp_uninstall.sh
@@ -9,12 +9,9 @@ install_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 service_location="/usr/lib/systemd/system/"
 etc_content="/etc/ssp"
 sbin_content="/usr/local/sbin/"
-# Gestion del servicio
-stop_daemon="sudo systemctl stop ssp.service"
-disable_daemon="sudo systemctl disable ssp.service"
 
-stop_daemon # Detener el servicio
-disable_daemon # Deshabilitar el servicio
+sudo systemctl stop ssp.service # Detener el servicio
+sudo systemctl disable ssp.service # Deshabilitar el servicio
 sudo rm -f "$service_location/ssp.service" # Borrar servicio
 sudo rm -rf "$etc_content" # Eliminar contenido de ruta /etc/ssp
 sudo rm -f "$sbin_content/ssp" # Eliminar fichero /usr/local/sbin/ssp
