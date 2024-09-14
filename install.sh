e628afb54c8d0c4a0957341622597e094a858668
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-09-14 18:10:14 +0200

    Memory use

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ install_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 allowed_services="/etc/ssp/allowed_services.txt"
 distro=$(lsb_release -is) # Obtener el Distributor ID
 edition="Unknown" # Inicializar la variable de tipo de edición
+install_end="/usr/local/sbin/ssp_"
 
 clear # Limpiar consola
 
@@ -27,32 +28,32 @@ else
     builtin exit 1
 fi
 
-builtin echo "Installing dependences..." # Simulación de tareas en el script
+builtin echo "Installing dependences from $install_dir" # Simulación de tareas en el script
 
 # Creacion de directorios del servicio
-sudo mkdir "/usr/local/sbin/ssp_" # Directorio de subprogramas
+sudo mkdir "$install_end" # Directorio de subprogramas
 sudo mkdir "/etc/ssp" # Directorio
 sudo mv "$install_dir/ssp_/ssp.sh" "/usr/local/sbin/ssp" # Instalacion de fichero ejecutable
-sudo mv "$install_dir/LICENSE.md" "/usr/local/sbin/ssp_/LICENSE.md" # Instalacion de licencia
-sudo mkdir "/usr/local/sbin/ssp_/py_service" # Creacion de ruta para scripts python
-sudo cp "$install_dir/ssp_/python_service/ssp.service.py" "/usr/local/sbin/ssp_/py_service/ssp.service.py" # Clonacion servicio y otorgacion de servicios
+sudo mv "$install_dir/LICENSE.md" "$install_end/LICENSE.md" # Instalacion de licencia
+sudo mkdir "$install_end/py_service" # Creacion de ruta para scripts python
+sudo cp "$install_dir/ssp_/python_service/ssp.service.py" "$install_end/py_service/ssp.service.py" # Clonacion servicio y otorgacion de servicios
 sudo cp "$install_dir/ssp_/necessaryservices/mainservices.txt" "$allowed_services" # Proceso de instalación de servicios obligatorios para del sistema
 sudo mkdir "/etc/ssp/logs" # Creacion de directorio destinado a los logs del servicio
-sudo cp "$install_dir/ssp_/ssp_uninstall.sh" "/usr/local/sbin/ssp_/" # Clona el fichero de desinstalacion
+sudo cp "$install_dir/ssp_/ssp_uninstall.sh" "$install_end/" # Clona el fichero de desinstalacion
 
-builtin echo "Configuring main files..."
+builtin echo "Configuring main files in $install_end"
 
 # Otorgar permisos 755 a todos los directorios y 644 a todos los archivos dentro de /usr/local/sbin/ssp_
-sudo find "/usr/local/sbin/ssp_" -type d -exec chmod 755 {} \;  # Directorios
-sudo find "/usr/local/sbin/ssp_" -type f -exec chmod 644 {} \;  # Archivos
-sudo chmod +x "/usr/local/sbin/ssp_/py_service/ssp.service.py" # Otorgar permisos de ejecución al script de servicio
+sudo find "$install_end" -type d -exec chmod 755 {} \;  # Directorios
+sudo find "$install_end" -type f -exec chmod 644 {} \;  # Archivos
+sudo chmod +x "$install_end/py_service/ssp.service.py" # Otorgar permisos de ejecución al script de servicio
 sudo chmod 755 "/usr/local/sbin/ssp" # Otorgar permisos al script principal
-sudo chmod 755 "/usr/local/sbin/ssp_/ssp_uninstall.sh" # Otorgar permisos al script de desinstalación
+sudo chmod 755 "$install_end/ssp_uninstall.sh" # Otorgar permisos al script de desinstalación
 sudo mkdir -p /etc/ssp/logs # Crear el directorio /etc/ssp/logs
 sudo chmod 755 /etc/ssp/logs # Otorgar permisos
 sudo chmod 755 /etc/ssp # Otorgar permisos al directorio /etc/ssp
 
-builtin echo "Configuring local & recommended services..."
+builtin echo "Configuring $localservices & recommended $install_dir/ssp_/recomendedservices/recomended.txt"
 
 builtin read -p "Do you want to install local services? [y/n]: " localservices # Proceso de instalacion de servicios locales (Para ubuntu)
 if [[ $localservices == "y" ]]; then
@@ -83,18 +84,17 @@ else
     builtin echo "Action cancelled."
 fi
 
-builtin echo "Creating service..."
+builtin echo "Creating service with $install_dir/ssp_/bash_file/systemd_contruct.sh"
 
 sudo bash "$install_dir/ssp_/bash_file/systemd_contruct.sh" # Llamar al generador de servicio
 
-builtin echo "Loading configuration file..."
-
-builtin echo "Liberating vairables..."
+builtin echo "Liberating vairables $install_dir, $allowed_services, $distro, $edition, $install_end"
 
 unset install_dir # Libera la variable después de usarla
 unset allowed_services # Libera la variable después de usarla
 unset distro # Libera la variable después de usarla
 unset edition # Libera la variable después de usarla
+unset install_end # Libera la variable después de usarla
 
 builtin echo "Loading daemon..."
 
