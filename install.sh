4dc1a9afd8160ed875a39b499f2b02c77c72273a
Nisamov <84858098+Nisamov@users.noreply.github.com>
2024-09-16 10:59:27 +0200

    Execute script alias - /USER fix

--- install.sh
+++ install.sh
@@ -79,8 +79,8 @@ if [[ $localservices == "y" ]]; then
         sed -i -e '$a\' "$allowed_services" # Asegurarse de que allowed_services termine con una nueva línea
         cat "$install_dir/ssp_/localservices/debian/localservices.txt" >> "$allowed_services" # Agrega los servicios de Debian
 
-        sed -i -e '$a\' "/home/$USER/.bashrc" # Añadir linea nueva en .bashrc
-        echo "alias ssp='sudo bash /usr/local/sbin/ssp'" >> "/home/$USER/.bashrc" # Agrega alias
+        sed -i -e '$a\' "/$USER/.bashrc" # Añadir linea nueva en .bashrc
+        echo "alias ssp='sudo bash /usr/local/sbin/ssp'" >> "/$USER/.bashrc" # Agrega alias
         source ~/.bashrc # Aplicar los cambios
     else
         builtin echo "There has been an error during installation."
